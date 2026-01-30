42d81733c310ea618906267dde7c0f8f2acfe5db
Omar Ahumada <omar.ahumadag@gmail.com>
2026-01-30 01:53:07 -0300

    Refactor code structure for improved readability and maintainability

---
+++ src/cypress.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  e2e: {
+    baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:8000',
+    specPattern: 'cypress/e2e/**/*.cy.{js,ts}'
+  },
+})
